ce4d26a5c15508953da32845876c3284475f1c14
ignacioLopezDev <ignaciolopez@outlook.com>
2022-09-09 17:49:30 -0300

    third commit

--- api/controllers/movies.js
+++ api/controllers/movies.js
@@ -4,24 +4,42 @@ const tmdb = "https://api.themoviedb.org/3/";
 const token =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzg0ZGQ0OGYyYjA1Mzk1OTZiNGEwZGQwNGQ2ODg5MSIsInN1YiI6IjYyOTIzYjY4ZGY4NmE4NzYyNjA0MzY2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R0Sb73FHKkXok5leGBSL_qQkJdO_sO6VhdaKv0P0eXI";
 const api_key = "api_key=a784dd48f2b0539596b4a0dd04d68891";
-const contentType = "application/json;charset=utf-8"
-
+const contentType = "application/json;charset=utf-8";
 
 // llamado a todas las movies
 exports.movies = (req, res) => {
   axios
-    .get(`${tmdb}discover/movie?${api_key}`, 
-    {
+    .get(`${tmdb}discover/movie?${api_key}`, {
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": contentType,
       },
-    }
-    )
+    })
     .then((movieData) => {
-      res.status(200).send(movieData.data);
+      res.status(200).send(movieData.data.results);
+      // res.status(200).send(movieData.data.results.map((result)=>result.title));
     })
     .catch((err) => {
-      res.status(500).send(err)
+      res.status(500).send(err);
     });
 };
+
+// llamado a una movie en particular por su id
+exports.movieId = (req, res) => {
+  const { id } = req.params;
+  axios
+    .get(`${tmdb}/movie/${id}?${api_key}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": contentType,
+      },
+    })
+    .then((movie) => {
+      res.status(200).send(movie.data.title);
+    })
+    .catch((err) => {
+      res.status(505).send(err);
+    });
+};
+
+
--- api/routes/movies.js
+++ api/routes/movies.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const movies = express.Router();
 
+// Llamado a la API externa
 const moviesController = require("../controllers/movies");
 
+// Grid de Movies
 movies.get("/", moviesController.movies);
+
+// Llamado a Movie en particular por su id
+movies.get("/:id", moviesController.movieId)
+
 
 module.exports = movies;
